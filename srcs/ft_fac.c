2458bee3c0f5ed418dcd9510f5658085990abc3f
Anselme GRUMBACH <agrumbac@e3r7p2.42.fr>
2016-11-18 22:21:39 +0100

    more maths fixes

--- srcs/ft_fac.c
+++ srcs/ft_fac.c
@@ -6,13 +6,13 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/18 16:50:34 by agrumbac          #+#    #+#             */
-/*   Updated: 2016/11/18 17:43:06 by agrumbac         ###   ########.fr       */
+/*   Updated: 2016/11/18 18:27:00 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_fac(int n)
 {
-	if (n < 0)
+	if (n < 0 || n > 12)
 		return (-1);
 	if (n == 1)
 		return (1);
