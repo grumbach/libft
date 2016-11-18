4077b0e868492aa8024c0c68edacba3ff7cbb804
Anselme GRUMBACH <agrumbac@e3r7p2.42.fr>
2016-11-18 17:18:35 +0100

    added more maths

---
+++ srcs/ft_pow.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pow.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/18 16:33:10 by agrumbac          #+#    #+#             */
+/*   Updated: 2016/11/18 17:03:40 by agrumbac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_pow(int n, int power)
+{
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	else if (power == 1)
+		return (n);
+	else
+		return (n * ft_recursive_power(n, power - 1));
+}
