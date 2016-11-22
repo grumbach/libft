0a6573ac092f4ed1f687d6921301ccf3f6e02fc7
Anselme GRUMBACH <agrumbac@e3r11p13.42.fr>
2016-11-22 20:54:40 +0100

    fixed bug generated by phatomfix

--- srcs/ft_lstmap.c
+++ srcs/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 10:08:03 by agrumbac          #+#    #+#             */
-/*   Updated: 2016/11/22 18:42:24 by agrumbac         ###   ########.fr       */
+/*   Updated: 2016/11/22 20:52:31 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 static t_list	*ft_lstaddender(t_list *alst, t_list *new)
 {
 	if (alst->next != NULL)
-		ft_lstaddender(&alst->next, new);
+		ft_lstaddender(alst->next, new);
 	else
 		alst->next = new;
 	return (alst);
