c79de2370335084a157207e68428f9e314e66315
Anselme GRUMBACH <agrumbac@e3r11p14.42.fr>
2016-12-30 01:16:12 +0100

    fixed ft_strlen_utf8 include

--- srcs/ft_str/ft_strlen_utf8.c
+++ srcs/ft_str/ft_strlen_utf8.c
@@ -6,11 +6,11 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/30 01:10:04 by agrumbac          #+#    #+#             */
-/*   Updated: 2016/12/30 01:12:04 by agrumbac         ###   ########.fr       */
+/*   Updated: 2016/12/30 01:15:22 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include <stdlib.h>
 
 size_t		ft_strlen_utf8(wchar_t *s)
 {
